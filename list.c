1bef415b2c78ea36d66a4756ba8d7f10f02d7f41
pavel-didin <65916221+pavel-didin@users.noreply.github.com>
2020-05-25 20:48:25 +0300

    Create list.c

---
+++ list.c
@@ -0,0 +1,14 @@
+CC=gcc
+CFLAGS= -Wall -g -fprofile-arcs -ftest-coverage
+
+all: tests
+tests.o: tests.c
+	${CC} -c tests.c -o tests.o ${CFLAGS}
+list.o: list.c
+	${CC} -c list.c -o list.o ${CFLAGS}
+tests: tests.o list.o
+	${CC} list.o tests.o -o tests ${CFLAGS}
+clean:
+	@rm -rf *.o *.gcno *.gcda *.gcov tests 2&>/dev/null
+tar:
+	tar czvf ../stack.tgz ../list
