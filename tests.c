0a65ed0e240a735ca943794ed64514e3c614e173
pavel-didin <65916221+pavel-didin@users.noreply.github.com>
2020-05-26 12:51:30 +0300

    Update tests.c

--- tests.c
+++ tests.c
@@ -3,42 +3,194 @@
 
 int main(void)
 {
-	List *list = NULL;
-	LIST_ERR err;
-
-  list_append(list, 5, &err);
-  
-  list_append(list, 6, &err);
-  
-  list_append(list, 59, &err);
-  
-  list_append(list, 17, &err);
-  
-  list_append(list, 8, &err);
-  
-  list_add(list, 3, &err);
-  
+  List *list = NULL;
+  LIST_ERR err;
+	
   list_destroy(list, &err);
-  
-  list_reverse_print(list, &err);
-  
-  list_print(list, &err);
-  
+	
+  if(err == ESIZE)
+	  fprintf(stdout, "Test_1\t->\tPASSED\n");
+  else
+	  fprintf(stdout, "Test_1\t->\tFAILED\n");
+	
+  list_insert(list, 1, 7, &err);
+	
+  if(err == ESIZE)
+	  fprintf(stdout, "Test_2\t->\tPASSED\n");
+  else
+	  fprintf(stdout, "Test_2\t->\tFAILED\n");
+	
   list_delete(list, 1, &err);
-  
+	
+  if(err == ESIZE)
+	  fprintf(stdout, "Test_3\t->\tPASSED\n");
+  else
+	  fprintf(stdout, "Test_3\t->\tFAILED\n");
+	
   list_delete_first(list, &err);
+	
+  if(err == ESIZE)
+	  fprintf(stdout, "Test_4\t->\tPASSED\n");
+  else
+	  fprintf(stdout, "Test_4\t->\tFAILED\n");
   
   list_delete_last(list, &err);
+	
+  if(err == ESIZE)
+	  fprintf(stdout, "Test_5\t->\tPASSED\n");
+  else
+	  fprintf(stdout, "Test_5\t->\tFAILED\n");
+	
+  if(list_append(list, 5, &err))
+	  fprintf(stdout, "Test_6\t->\tFAILED\n");
+  else
+	  fprintf(stdout, "Test_6\t->\tPASSED\n");
+
+  if(list_append(list, 6, &err))
+	  fprintf(stdout, "Test_7\t->\tFAILED\n");
+  else
+	  fprintf(stdout, "Test_7\t->\tPASSED\n");
   
-  list_insert(list, 1, 7, &err);
+  if(list_append(list, 19, &err))
+	  fprintf(stdout, "Test_8\t->\tFAILED\n");
+  else
+	  fprintf(stdout, "Test_8\t->\tPASSED\n");
+  
+  if(list_append(list, 17, &err))
+	  fprintf(stdout, "Test_9\t->\tFAILED\n");
+  else
+	  fprintf(stdout, "Test_9\t->\tPASSED\n");
+  
+  if(list_append(list, 8, &err))
+	  fprintf(stdout, "Test_10\t->\tFAILED\n");
+  else
+	  fprintf(stdout, "Test_10\t->\tPASSED\n");
+  
+  if(list_add(list, 25, &err))
+	  fprintf(stdout, "Test_11\t->\tFAILED\n");
+  else
+	  fprintf(stdout, "Test_11\t->\tPASSED\n");
+  
+  list_reverse_print(list, &err);
+	
+  if(err == ESUCCESS)
+	  fprintf(stdout, "Test_12\t->\tPASSED\n");
+  else
+	  fprintf(stdout, "Test_12\t->\tFAILED\n");
+  
+  list_print(list, &err);
+	
+  if(err == ESUCCESS)
+	  fprintf(stdout, "Test_13\t->\tPASSED\n");
+  else
+	  fprintf(stdout, "Test_13\t->\tFAILED\n");
+  
+  list_delete(list, 100, &err);
+	
+  if(err == ESIZE)
+	  fprintf(stdout, "Test_14\t->\tPASSED\n");
+  else
+	  fprintf(stdout, "Test_14\t->\tFAILED\n");
+	
+  list_delete(list, 2, &err);
+	
+  if(err == ESUCCESS)
+	  fprintf(stdout, "Test_15\t->\tPASSED\n");
+  else
+	  fprintf(stdout, "Test_15\t->\tFAILED\n");
+  
+  list_delete_first(list, &err);
+	
+  if(err == ESUCCESS)
+	  fprintf(stdout, "Test_16\t->\tPASSED\n");
+  else
+	  fprintf(stdout, "Test_16\t->\tFAILED\n");
+  
+  list_delete_last(list, &err);
+	
+  if(err == ESUCCESS)
+	  fprintf(stdout, "Test_17\t->\tPASSED\n");
+  else
+	  fprintf(stdout, "Test_17\t->\tFAILED\n");
+  
+  list_insert(list, 100, 7, &err);
+	
+  if(err == ESIZE)
+	  fprintf(stdout, "Test_18\t->\tPASSED\n");
+  else
+	  fprintf(stdout, "Test_18\t->\tFAILED\n");
   
   prinft("%d\n", list_get(list, 0, &err));
+	
+  list_insert(list, 2, 9, &err);
+	
+  if(err == ESUCCESS)
+	  fprintf(stdout, "Test_19\t->\tPASSED\n");
+  else
+	  fprintf(stdout, "Test_19\t->\tFAILED\n");
   
-  list_set(list, 1, 4, &err);
+  list_get(list, 0, &err);
+	
+  if(err == ESUCCESS)
+	  fprintf(stdout, "Test_20\t->\tPASSED\n");
+  else
+	  fprintf(stdout, "Test_20\t->\tFAILED\n");
+	
+  list_get(list, 100, &err);
+	
+  if(err == ESIZE)
+	  fprintf(stdout, "Test_21\t->\tPASSED\n");
+  else
+	  fprintf(stdout, "Test_21\t->\tFAILED\n");
+	
+  list_get(list, 2, &err);
+	
+  if(err == ESUCCESS)
+	  fprintf(stdout, "Test_22\t->\tPASSED\n");
+  else
+	  fprintf(stdout, "Test_22\t->\tFAILED\n");
+	
+  list_set(list, 100, 4, &err);
+	
+  if(err == ESIZE)
+	  fprintf(stdout, "Test_23\t->\tPASSED\n");
+  else
+	  fprintf(stdout, "Test_23\t->\tFAILED\n");
+	
+  list_set(list, 1, 99, &err);
+	
+  if(err == ESUCCESS)
+	  fprintf(stdout, "Test_24\t->\tPASSED\n");
+  else
+	  fprintf(stdout, "Test_24\t->\tFAILED\n");
   
-  printf("%d\n", list_find(list, 4, &err));
+  list_find(list, 97, &err);
+	
+  if(err == EFOUND)
+	  fprintf(stdout, "Test_25\t->\tPASSED\n");
+  else
+	  fprintf(stdout, "Test_25\t->\tFAILED\n");
+	
+  list_find(list, 99, &err);
+	
+  if(err == ESUCCESS)
+	  fprintf(stdout, "Test_26\t->\tPASSED\n");
+  else
+	  fprintf(stdout, "Test_26\t->\tFAILED\n");
   
-  printf("%u\n", list_size(list, &err));
-
-	return 0;
+  list_size(list, &err);
+	
+  if(err == ESUCCESS)
+	  fprintf(stdout, "Test_27\t->\tPASSED\n");
+  else
+	  fprintf(stdout, "Test_27\t->\tFAILED\n");
+	
+  list_destroy(list, &err);
+	
+  if(err == ESUCCESS)
+	  fprintf(stdout, "Test_28\t->\tPASSED\n");
+  else
+	  fprintf(stdout, "Test_28\t->\tFAILED\n");
+	
+  return 0;
 }
